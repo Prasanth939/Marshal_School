959a32b4c8d2bfdce6b5db896d64ac306f752191
villa <villaprasanth1@gmail.com>
2024-01-22 15:55:26 +0530

    teacher changed

--- MARSHALL SCHOOL/Models/Student.cs
+++ MARSHALL SCHOOL/Models/Student.cs
@@ -12,7 +12,7 @@
         public string Address { get; set; }
         public string Phone { get; set; }
         public int Standard { get; set; }
-        public string Teacher { get; set; }
+        public string Teacher2 { get; set; }
 
     }
 }
