76bbb14de6f0d71b6d847541bdc96d1bf7d2e70b
villa <villaprasanth1@gmail.com>
2024-01-22 17:39:51 +0530

    Attendance Class And Repository

---
+++ MARSHALL SCHOOL/Models/Attendance.cs
@@ -0,0 +1,11 @@
+﻿namespace MARSHALL_SCHOOL.Models
+{
+    public class Attendance
+    {
+        public int AttendanceId { get; set; }
+        public int StudentId { get; set;}
+        public DateTime Date {  get; set;}
+        public bool Attend { get; set;}
+
+    }
+}
---
+++ MARSHALL SCHOOL/Models/AttendenceRepository.cs
@@ -0,0 +1,44 @@
+﻿namespace MARSHALL_SCHOOL.Models
+{
+    public class AttendenceRepository
+    {
+        static List<Attendance> Attended = new List<Attendance>()
+        {
+            new Attendance()
+            {
+                AttendanceId = 1,
+                StudentId=11,
+                Date= new DateTime(13-11-2023),
+                Attend=true
+            },
+             new Attendance()
+            {
+                AttendanceId = 2,
+                StudentId=12,
+                Date=new DateTime(12-12-2023),
+                Attend=true
+            },
+              new Attendance()
+            {
+                AttendanceId = 1,
+                StudentId=13,
+                Date= new DateTime(03-01-2024),
+                Attend=false
+            },
+               new Attendance()
+            {
+                AttendanceId = 1,
+                StudentId=14,
+                Date= new DateTime(09-01-2024),
+                Attend=true
+            },
+                new Attendance()
+            {
+                AttendanceId = 1,
+                StudentId=15,
+                Date= new DateTime(05-01-2024),
+                Attend=false
+            },
+        };
+    }
+}
