93c45727fa37042fc7634cfcd4b2aef5a809644f
villa <villaprasanth1@gmail.com>
2024-01-22 15:51:21 +0530

    Phone is changrd

--- MARSHALL SCHOOL/Models/Student.cs
+++ MARSHALL SCHOOL/Models/Student.cs
@@ -10,7 +10,7 @@
         public string FatherName { get; set; }
         public string MotherName { get; set; }
         public string Address { get; set; }
-        public string MobileNumber { get; set; }
+        public string Phone { get; set; }
         public int Standard { get; set; }
 
     }
