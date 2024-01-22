1209018cdeab60cc0d5861fb818a48c6d46c3765
villa <villaprasanth1@gmail.com>
2024-01-22 14:58:47 +0530

    Marshla First Commit

---
+++ MARSHALL SCHOOL/Controllers/StudentController.cs
@@ -0,0 +1,58 @@
+﻿using MARSHALL_SCHOOL.Models;
+using MARSHALL_SCHOOL.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MARSHALL_SCHOOL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        StudentRepository stdRepo = new StudentRepository();
+        [HttpGet]
+        public IActionResult GetS()
+        {
+            var students = stdRepo.GetAllStudents();
+            return Ok(students);
+            
+        }
+        [HttpGet("Id")]
+        public IActionResult GetStudentbyid(int Id)
+
+        {
+            var students = stdRepo.GetAllStudents();
+            var student = students.FirstOrDefault(s => s.Id == Id);
+            if(student == null)
+            {
+                return BadRequest("There are no stundets with this ID!");
+            }
+            return Ok(student);
+        }
+        [HttpPost]  
+        public IActionResult CreateStudent(Student student)
+        {
+
+            var std = stdRepo.CreateStudent(student);
+            if(std == null)
+            {
+                return BadRequest("Unable to create Students");
+
+            }
+            return Ok(std);
+
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteStudent(int Id) {
+            var res = stdRepo.RemoveStudent(Id);
+            if(res == 0)
+            {
+                return NotFound("There are no stunts to delete!");
+            }
+            return Ok("Deleted Succssfully!");
+        }
+
+
+    }
+}
---
+++ MARSHALL SCHOOL/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace MARSHALL_SCHOOL.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public int RollNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string FatherName { get; set; }
+        public string MotherName { get; set; }
+        public string Address { get; set; }
+        public string MobileNumber { get; set; }
+        public int Class { get; set; }
+
+    }
+}
